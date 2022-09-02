dd0cb34bd9a05b1e58931bc96efef25e8d6bfc19
ProgrammerAuditore <victorvj098@gmail.com>
2022-09-02 17:23:02 -0500

    *** Agregado : Se creo la vista Principal y Crear usuario

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+
+import visPrincipal from './vistas/visPrincipal';
+import visCrearUsuario from './vistas/visCrearUsuario';
+
+
+function MyStack(){
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name='visPrincipal' component={visPrincipal} options={{title:'App Maximo'}} />
+            <Stack.Screen name='visCrearUsuario' component={visCrearUsuario} options={{title:'App Maximo'}} />
+        </Stack.Navigator>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+function App(){
+    return(
+        <NavigationContainer> 
+            <MyStack></MyStack> 
+        </NavigationContainer>
+    );
+}
+
+const styles=StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor:'#fff',
+        alignItems:'center',
+        justifyContent:'center'
+    }
 });
+
+export default App;
---
+++ vistas/visPrincipal.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const visPrincipal = () => {
+  return (
+      <Text>Hola mundo, desde react-native.</Text>
+  );
+}
+
+export default visPrincipal;
+
+const styles = StyleSheet.create({});
