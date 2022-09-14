f921e686f643dc8e108a261d53b6e1f145a1a5a7
ProgrammerAuditore <victorvj098@gmail.com>
2022-09-14 15:41:21 -0500

    *** Se creo la vista crear usuario

--- vistas/visCrearUsuario.js
+++ vistas/visCrearUsuario.js
@@ -1,9 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import { Card, Input, Text, Button } from '@rneui/base';
 
 const visCrearUsuario = (props) => {
   return (
-      <Text>Crear un usuario.</Text>
+      <ScrollView>
+        <View  style={{ flex: 1, flexDirection: "column"}}>
+          <Card>
+            <Card.Title>Crear usuario</Card.Title>
+            <Card.Divider></Card.Divider>
+            
+            {/* Campo: Nombre */}
+            <View>
+              <Text>Nombre</Text>
+              <Input 
+              placeholder='Ingresar nombre'
+              ></Input>
+            </View>
+
+            {/* Campo: Edad */}
+            <View>
+              <Text>Edad</Text>
+              <Input 
+              placeholder='Ingresar edad'
+              ></Input>
+            </View>
+
+            {/* Campo: Apellidos */}
+            <View>
+
+              <Text>Apellidos</Text>
+              <Input 
+              placeholder='Ingresar apellidos'
+              ></Input>
+            </View>
+
+            {/* Campo: Dirección */}
+            <View>
+              <Text>Dirección</Text>
+              <Input 
+              placeholder='Ingresar dirección'
+              ></Input>
+            </View>
+
+            {/* Botón */}
+            <Button size={"sm"} title="Crear usuario" onPress={() => props.navigation.navigate("vistListaUsuario")}></Button>
+
+          </Card>
+        </View>
+        
+      </ScrollView>
   );
 }
 
