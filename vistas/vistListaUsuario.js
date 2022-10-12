eacb88265f85fe92242683eb533d3ac30f0a0d65
ProgrammerAuditore <victorvj098@gmail.com>
2022-10-12 16:05:04 -0500

    *** dev : Se agrego listar usuario desde firebase *Funciona*

--- vistas/vistListaUsuario.js
+++ vistas/vistListaUsuario.js
@@ -2,49 +2,39 @@ import { StyleSheet, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
 import { ListItem, Avatar, Button, Text } from "@rneui/themed";
-
-const list = [
-  {
-    nombre: 'Amy Farha',
-    avatar_url: 'https://cdn-icons-png.flaticon.com/512/168/168734.png',
-    subtitulo: 'Vice President',
-    estado: 'Disponible 😅'
-  },
-  {
-    nombre: 'Chris Jackson',
-    avatar_url: 'https://cdn-icons-png.flaticon.com/512/147/147144.png',
-    subtitulo: 'Vice Chairman',
-    estado: 'Ocupado 😅'
-  },
-  {
-    nombre: 'Chris Jackson',
-    avatar_url: 'https://cdn-icons-png.flaticon.com/512/194/194938.png',
-    subtitulo: 'Vice Chairman',
-    estado: 'Desarrollando App 😅'
-  },
-  {
-    nombre: 'Chris Jackson',
-    avatar_url: 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
-    subtitulo: 'Vice Chairman',
-    estado: 'Ocupado 😅'
-  }
-];
+import firebase from '../firebase';
 
 const vistListaUsuario = (props) => {
 
-  const [currentDate, setCurrentDate] = useState('');
+  const [users,setUsers] = useState([]);
+
+  const fetchPosts = async () => {
+    try {
+      const users = [];
+      await firebase.conexion
+      .collection('bdMonitoreo') 
+      .get()
+      .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+            const {
+              usuId,
+              usuNombres,
+            } = doc.data();
+            users.push({
+              id: doc.id,
+              usuId,
+              usuNombres,
+            });
+          });
+        });
+        setUsers(users);
+      } catch (e) {
+        alert(e);
+      }
+  };
 
   useEffect(() => {
-    var date = new Date().getDate(); //Current Date
-    var month = new Date().getMonth() + 1; //Current Month
-    var year = new Date().getFullYear(); //Current Year
-    var hours = new Date().getHours(); //Current Hours
-    var min = new Date().getMinutes(); //Current Minutes
-    var sec = new Date().getSeconds(); //Current Seconds
-    setCurrentDate(
-      date + '/' + month + '/' + year
-      + ' ' + hours + ':' + min + ':' + sec
-    );
+    fetchPosts();
   }, []);
 
   return (
@@ -52,22 +42,35 @@ const vistListaUsuario = (props) => {
       <Button size={"sm"} title="Crear usuario" onPress={() => props.navigation.navigate("visCrearUsuario")}></Button>
       <View>
         {
-          list.map((l, i) => (
-            <ListItem 
-            onPress={() => props.navigation.navigate("visDetalleUsuario")}
-            key={i} bottomDivider>
-              <Avatar
-                size={"large"}
-                rounded
-                source={{ uri: l.avatar_url }} />
-              <ListItem.Content>
-                <ListItem.Title>{l.nombre} #{i}</ListItem.Title>
-                <ListItem.Subtitle>{l.subtitulo}</ListItem.Subtitle>
-                <Text>{currentDate}</Text>
-              </ListItem.Content>
+          users.map((user) => {
+            return (
+             <ListItem  key={user.id} 
+             bottomDivider 
+             //onPress= {()=> alert("Su Id es: "+user.id)}
+             // onPress= {()=> props.navigation.navigate('UserDetailScreen', {
+             //   userId: user.id,
+             //   userName: user.name,
+             //   userMail: user.mail,
+             //   userPhone: user.phone, 
+             //   } ) }
+            >  
+               <ListItem.Chevron />
+               <Avatar
+                   rounded title ="usr"
+                   size="large"
+                   source={{uri:'https://randomuser.me/api/portraits/men/36.jpg',
+                   }}
+             />
+               <ListItem.Content>
+                    <ListItem.Title>{user.usuId}</ListItem.Title>
+                    <ListItem.Subtitle>{user.usuNombres}</ListItem.Subtitle>
+               </ListItem.Content>
             </ListItem>
-          ))
-        }
+            
+             );
+
+         })
+        } 
       </View>
     </ScrollView>
   )
